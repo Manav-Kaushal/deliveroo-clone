f8bc7fe9f7c4ede2e67c522b17148ef3484adca4
Manav Kaushal <manavkaushal756@gmail.com>
2022-07-13 03:21:24 +0530

    redux integrated

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -2,6 +2,7 @@ import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { LocationMarkerIcon, StarIcon } from "react-native-heroicons/outline";
 import { urlFor } from "../sanity";
+import { useNavigation } from "@react-navigation/native";
 
 const RestaurantCard = ({
   id,
@@ -15,8 +16,26 @@ const RestaurantCard = ({
   longitude,
   latitude,
 }) => {
+  const navigation = useNavigation();
+
   return (
-    <TouchableOpacity className="bg-white mr-3 mb-3 shadow rounded-md">
+    <TouchableOpacity
+      className="bg-white mr-3 mb-3 shadow rounded-md"
+      onPress={() => {
+        navigation.navigate("Restaurant", {
+          id,
+          imgUrl,
+          title,
+          rating,
+          genre,
+          address,
+          shortDescription,
+          dishes,
+          longitude,
+          latitude,
+        });
+      }}
+    >
       <Image
         source={{
           uri: urlFor(imgUrl)?.url(),
